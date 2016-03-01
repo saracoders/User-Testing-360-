72731d9ba16118e805c8e08bba27ef6b9711757d
saracoders <nolasco.f.sara@gmail.com>
2016-03-01 16:40:11 -0500

    user-testing-360

---
+++ js/menu-scroll.js
@@ -0,0 +1,21 @@
+$(document).scroll(function() {    
+    var scroll = $(this).scrollTop();
+
+    if (scroll >= 100) {
+        $("#menu").addClass("active");
+    } else {
+        $("#menu").removeClass("active");
+    }
+
+});
+
+
+
+$(".navbar-toggle").click(function(){
+  $(this).toggleClass("open");
+});
+
+
+
+
+
